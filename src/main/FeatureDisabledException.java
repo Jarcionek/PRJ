03e539e25d2e3df3243be7de10e52779f91cae07
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2012-11-14 22:53:22 +0000

    added untracked file

---
+++ src/main/FeatureDisabledException.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * @author Jaroslaw Pawlak
+ */
+public class FeatureDisabledException extends RuntimeException {
+
+    public FeatureDisabledException(String message) {
+        super(message);
+    }
+
+    public FeatureDisabledException() {
+    }
+
+}
