c93ec01ec9ea646e480bb3e9aef5af135b07c2bd
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2013-02-19 22:12:37 +0000

    renaming

--- src/network/GUI/Mode.java
+++
@@ -1,9 +0,0 @@
-package network.GUI;
-
-/**
- * @author Jaroslaw Pawlak
- */
-public enum Mode {
-    CREATOR,
-    SIMULATION,
-}
---
+++ src/network/GUI/simulation/C.java
@@ -0,0 +1,14 @@
+package network.GUI;
+
+/**
+ * Constants.
+ * 
+ * @author Jaroslaw Pawlak
+ */
+class C {
+    /**
+     * Oval sizes (diameter) of nodes drawn in network
+     */
+    static final int S = 20;
+
+}
